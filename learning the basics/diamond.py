7613c6f716798589ff69787ac8fea8b18b2af9b6
sivakumar0412 <sivakumar041203@gmail.com>
2026-01-07 21:30:32 +0530

    siva

---
+++ learning the basics/diamond.py
@@ -0,0 +1,15 @@
+n = 4
+# Upper
+for i in range(1, n+1):
+    for j in range(1, n-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        print("*", end="")
+    print()
+# Lower
+for i in range(n-1, 0, -1):
+    for j in range(1, n-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        print("*", end="")
+    print()

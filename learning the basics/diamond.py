74db9fff4f38c4c264871be691fd0f3ecd80152a
sivakumar0412 <sivakumar041203@gmail.com>
2026-01-09 22:02:57 +0530

    new

--- learning the basics/diamond.py
+++ learning the basics/diamond.py
@@ -13,3 +13,28 @@ for i in range(n-1, 0, -1):
     for j in range(1, 2*i):
         print("*", end="")
     print()
+
+
+# Using FUNCTION
+def diamond(n):
+    for i in range(1, n+1):
+        print(" "*(n-i) + "*"*(2*i-1))
+    for i in range(n-1, 0, -1):
+        print(" "*(n-i) + "*"*(2*i-1))
+
+
+diamond(4)
+
+
+# Using WHILE loop
+n = 4
+i = 1
+
+while i <= n:
+    print(" "*(n-i) + "*"*(2*i-1))
+    i += 1
+
+i = n-1
+while i >= 1:
+    print(" "*(n-i) + "*"*(2*i-1))
+    i -= 1

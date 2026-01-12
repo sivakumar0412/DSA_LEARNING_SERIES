b73fde17a52efa4320c0e26e36c27d785b3e75f7
sivakumar0412 <sivakumar041203@gmail.com>
2026-01-12 23:01:56 +0530

    Add

--- learning the basics/reversed_pyramid_pattern.py
+++ learning the basics/reversed_pyramid_pattern.py
@@ -7,3 +7,14 @@ for i in range(rows, -1, -1):
     for j in range(0, i + 1):
         print("*", end=" ")
     print("")
+
+# Using While Loop
+rows = 5
+i = 1
+while i <= rows:
+    j = 1
+    while j <= i:
+        print("*", end=" ")
+        j += 1
+    print()
+    i += 1

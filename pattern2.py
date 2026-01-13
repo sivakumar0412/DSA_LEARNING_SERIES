9154e8cad780b11d49e3dd382bfebd0bfda5b500
sivakumar0412 <sivakumar041203@gmail.com>
2026-01-13 22:16:44 +0530

    add

--- pattern2.py
+++ pattern2.py
@@ -4,3 +4,10 @@ for i in range(n):
     for j in range(i):
         print("*", end="")
     print()
+
+# square pattern 
+def square(rows):
+    for i in range(rows):
+        print("* " * rows)
+
+square(5)

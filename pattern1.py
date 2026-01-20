c2966d5c5fdc805800f975cba0fbc9a48f127ce6
sivakumar0412 <sivakumar041203@gmail.com>
2026-01-20 22:12:39 +0530

    ADD

--- pattern1.py
+++ pattern1.py
@@ -8,3 +8,12 @@ for i in range(n):
 # 5x5 star pattern
 for i in range(5):
     print("*" * 5)
+
+# Count digits in a number
+num = 12345
+count = 0
+
+for i in str(num):
+    count += 1
+
+print(count)

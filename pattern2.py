3b9db0555dcb01b508e167c65fb4be73a2d1b3a8
sivakumar0412 <sivakumar041203@gmail.com>
2026-01-21 21:25:25 +0530

    ADD

--- pattern2.py
+++ pattern2.py
@@ -11,3 +11,28 @@ def square(rows):
         print("* " * rows)
 
 square(5)
+
+# Stock span problem
+
+
+class Solution:
+    def calculateSpan(self, arr):
+        n = len(arr)
+        span = [0] * n
+        stack = []   # stores indices
+
+        for i in range(n):
+            # Remove smaller or equal prices
+            while stack and arr[stack[-1]] <= arr[i]:
+                stack.pop()
+
+            # If stack empty, span is full length
+            if not stack:
+                span[i] = i + 1
+            else:
+                span[i] = i - stack[-1]
+
+            # Push current index
+            stack.append(i)
+
+        return span

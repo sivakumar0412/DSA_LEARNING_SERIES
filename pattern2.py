4a9b571f86b8a83dcb8f5f0f80baf4bd92817455
sivakumar0412 <sivakumar041203@gmail.com>
2026-01-25 20:48:30 +0530

    ADD

--- pattern2.py
+++ pattern2.py
@@ -112,3 +112,21 @@ class Solution:
             survivor = (survivor + k) % i
 
         return survivor + 1
+
+# Number of Valid Parentheses
+
+
+class Solution:
+    def findWays(self, n):
+        if n % 2 != 0:
+            return 0
+
+        pairs = n // 2
+        dp = [0] * (pairs + 1)
+        dp[0] = 1
+
+        for i in range(1, pairs + 1):
+            for j in range(i):
+                dp[i] += dp[j] * dp[i - 1 - j]
+
+        return dp[pairs]

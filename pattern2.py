92519122e8ed4e708e3fe1c8f9c661920284004d
sivakumar0412 <sivakumar041203@gmail.com>
2026-02-01 03:07:03 +0530

    ADD

--- pattern2.py
+++ pattern2.py
@@ -290,3 +290,26 @@ class kQueues:
         return self.free == -1
 
 
+# K Sized Subarray Maximum
+class Solution:
+    def maxOfSubarrays(self, arr, k):
+        dq = deque()
+        result = []
+
+        for i in range(len(arr)):
+
+            # Remove indexes that are out of this window
+            if dq and dq[0] <= i - k:
+                dq.popleft()
+
+            # Remove smaller elements from back
+            while dq and arr[dq[-1]] < arr[i]:
+                dq.pop()
+
+            dq.append(i)
+
+            # When first window is completed
+            if i >= k - 1:
+                result.append(arr[dq[0]])
+
+        return result

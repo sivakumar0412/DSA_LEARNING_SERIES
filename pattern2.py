411c72d47ca5a286b62043fd53e949fcc9d3b0d9
sivakumar0412 <sivakumar041203@gmail.com>
2026-02-17 22:19:23 +0530

    ADD

--- pattern2.py
+++ pattern2.py
@@ -603,3 +603,38 @@ class Solution:
                 return False
 
         return True
+
+#Maximum number of overlapping Intervals
+
+
+class Solution:
+    def overlapInt(self, arr):
+        n = len(arr)
+
+        start = []
+        end = []
+
+        for s, e in arr:
+            start.append(s)
+            end.append(e)
+
+        start.sort()
+        end.sort()
+
+        i = 0
+        j = 0
+        curr = 0
+        max_overlap = 0
+
+        while i < n and j < n:
+            # Since intervals are inclusive,
+            # start <= end means overlap
+            if start[i] <= end[j]:
+                curr += 1
+                max_overlap = max(max_overlap, curr)
+                i += 1
+            else:
+                curr -= 1
+                j += 1
+
+        return max_overlap

101d91673635e0fb18ad7c01d30a1866d5266adb
sivakumar0412 <sivakumar041203@gmail.com>
2026-02-19 20:48:10 +0530

    ADD

--- pattern2.py
+++ pattern2.py
@@ -674,3 +674,17 @@ class Solution:
 
         _, count = merge_sort(arr)
         return count
+
+# Missing Element in Range
+
+
+class Solution:
+    def missingRange(self, arr, low, high):
+        s = set(arr)
+        result = []
+
+        for num in range(low, high + 1):
+            if num not in s:
+                result.append(num)
+
+        return result

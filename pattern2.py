ad10830cc5819b18825643708db31793e0a77876
sivakumar0412 <sivakumar041203@gmail.com>
2026-02-28 22:34:05 +0530

    S

--- pattern2.py
+++ pattern2.py
@@ -828,3 +828,32 @@ class Solution:
                 map2[c2] = c1
 
         return True
+
+# Find the closest pair from two arrays
+
+
+class Solution:
+    def findClosestPair(self, arr1, arr2, x):
+        n = len(arr1)
+        m = len(arr2)
+
+        i = 0
+        j = m - 1
+
+        min_diff = float('inf')
+        result = [0, 0]
+
+        while i < n and j >= 0:
+            curr_sum = arr1[i] + arr2[j]
+            diff = abs(curr_sum - x)
+
+            if diff < min_diff:
+                min_diff = diff
+                result = [arr1[i], arr2[j]]
+
+            if curr_sum > x:
+                j -= 1
+            else:
+                i += 1
+
+        return result

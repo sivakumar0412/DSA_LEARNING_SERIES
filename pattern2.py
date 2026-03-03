c7d4db258f725d8024d434fce7fe70efec11958c
sivakumar0412 <sivakumar041203@gmail.com>
2026-03-03 23:23:07 +0530

    AD

--- pattern2.py
+++ pattern2.py
@@ -903,3 +903,26 @@ class Solution:
                 right -= 1
 
         return water
+
+# Longest subarray with Atmost two distinct integers
+class Solution:
+    def totalElements(self, arr):
+        left = 0
+        freq = {}
+        max_len = 0
+
+        for right in range(len(arr)):
+            # Add current element
+            freq[arr[right]] = freq.get(arr[right], 0) + 1
+
+            # If more than 2 distinct, shrink window
+            while len(freq) > 2:
+                freq[arr[left]] -= 1
+                if freq[arr[left]] == 0:
+                    del freq[arr[left]]
+                left += 1
+
+            # Update maximum length
+            max_len = max(max_len, right - left + 1)
+
+        return max_len

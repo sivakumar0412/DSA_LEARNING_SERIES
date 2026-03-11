929f7195f095645bc5bd934276f83446b15c3b4b
sivakumar0412 <sivakumar041203@gmail.com>
2026-03-11 19:45:39 +0530

    S

--- pattern2.py
+++ pattern2.py
@@ -1084,3 +1084,40 @@ class Solution:
             ans += n - idx
         
         return ans
+
+# Sum of subarray minimums
+
+
+class Solution:
+    def sumSubMins(self, arr):
+        n = len(arr)
+
+        prev = [-1]*n
+        next_ = [n]*n
+
+        stack = []
+
+        # Previous Less Element
+        for i in range(n):
+            while stack and arr[stack[-1]] > arr[i]:
+                stack.pop()
+            prev[i] = stack[-1] if stack else -1
+            stack.append(i)
+
+        stack = []
+
+        # Next Less Element
+        for i in range(n-1, -1, -1):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            next_[i] = stack[-1] if stack else n
+            stack.append(i)
+
+        ans = 0
+
+        for i in range(n):
+            left = i - prev[i]
+            right = next_[i] - i
+            ans += arr[i] * left * right
+
+        return ans

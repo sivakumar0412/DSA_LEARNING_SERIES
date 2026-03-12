d16fe83cbaa8aaccc6ce5439e9409bed2da271ef
sivakumar0412 <sivakumar041203@gmail.com>
2026-03-12 18:41:18 +0530

    S

--- pattern2.py
+++ pattern2.py
@@ -1121,3 +1121,31 @@ class Solution:
             ans += arr[i] * left * right
 
         return ans
+
+# Minimum K Consecutive Bit Flips
+
+
+class Solution:
+    def kBitFlips(self, arr, k):
+        n = len(arr)
+        is_flipped = [0] * n
+        flip = 0
+        ans = 0
+
+        for i in range(n):
+            # remove effect of flip window that ended
+            if i >= k:
+                flip ^= is_flipped[i - k]
+
+            # current bit after flips
+            current = arr[i] ^ flip
+
+            # if current bit is 0, we must flip here
+            if current == 0:
+                if i + k > n:
+                    return -1
+                is_flipped[i] = 1
+                flip ^= 1
+                ans += 1
+
+        return ans

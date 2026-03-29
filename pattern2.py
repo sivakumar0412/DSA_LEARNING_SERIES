4012046bf8c27a1cf7cf3b56faedb6684294b2cd
sivakumar0412 <sivakumar041203@gmail.com>
2026-03-29 21:36:07 +0530

    :S:

--- pattern2.py
+++ pattern2.py
@@ -1668,3 +1668,26 @@ class Solution:
         result = [i for i in range(V) if ap[i]]
 
         return result if result else [-1]
+
+# Partitions with Given Difference
+
+
+class Solution:
+    def countPartitions(self, arr, diff):
+        total = sum(arr)
+
+        # not possible
+        if (total + diff) % 2 != 0:
+            return 0
+
+        target = (total + diff) // 2
+
+        # DP array
+        dp = [0] * (target + 1)
+        dp[0] = 1   # one way to make sum 0
+
+        for num in arr:
+            for j in range(target, num - 1, -1):
+                dp[j] += dp[j - num]
+
+        return dp[target]

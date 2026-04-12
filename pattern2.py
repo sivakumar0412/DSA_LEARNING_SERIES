1517ff822db357d3637eca3c72e9075ce37ec5bf
sivakumar0412 <sivakumar041203@gmail.com>
2026-04-12 21:38:37 +0530

    S

--- pattern2.py
+++ pattern2.py
@@ -1895,3 +1895,17 @@ class Solution:
                 j += 1
         
         return result
+# Toeplitz matrix
+
+
+class Solution:
+    def isToeplitz(self, mat):
+        rows = len(mat)
+        cols = len(mat[0])
+
+        # Check diagonals
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if mat[i][j] != mat[i-1][j-1]:
+                    return False
+        return True

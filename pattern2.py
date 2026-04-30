d944ad854aa7949883676966556e21813fef1b49
sivakumar0412 <sivakumar041203@gmail.com>
2026-04-30 21:40:04 +0530

    s

--- pattern2.py
+++ pattern2.py
@@ -2150,3 +2150,23 @@ class Solution:
         
         # Minimum swaps = missing ones in best window
         return total_ones - max_ones
+
+# Check if an Array is Max Heap
+class Solution:
+    def isMaxHeap(self, arr):
+        n = len(arr)
+        
+        # Only check non-leaf nodes
+        for i in range(n // 2):
+            left = 2 * i + 1
+            right = 2 * i + 2
+            
+            # Check left child
+            if left < n and arr[i] < arr[left]:
+                return False
+            
+            # Check right child
+            if right < n and arr[i] < arr[right]:
+                return False
+        
+        return True

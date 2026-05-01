ee3cb3917c54493e8a92356d13484747dc212785
sivakumar0412 <sivakumar041203@gmail.com>
2026-05-01 18:34:40 +0530

    s

--- pattern2.py
+++ pattern2.py
@@ -2170,3 +2170,24 @@ class Solution:
                 return False
         
         return True
+
+# Kth Largest in a Stream
+import heapq
+
+class Solution:
+    def kthLargest(self, arr, k):
+        heap = []
+        result = []
+        
+        for num in arr:
+            heapq.heappush(heap, num)
+            
+            if len(heap) > k:
+                heapq.heappop(heap)
+            
+            if len(heap) < k:
+                result.append(-1)
+            else:
+                result.append(heap[0])
+        
+        return result

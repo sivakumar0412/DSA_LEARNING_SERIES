a73966ccd9e5670b5b938d243a7b02f6470f34a9
sivakumar0412 <sivakumar041203@gmail.com>
2026-06-10 13:56:11 +0530

    add

--- pattern2.py
+++ pattern2.py
@@ -2777,3 +2777,36 @@ class Solution:
         # Step 3: Reverse again to restore order
         head = self.reverse(head)
         return head
+
+
+# Binary Searchable Count
+class Solution:
+    def binarySearchable(self, arr):
+        n = len(arr)
+        ans = 0
+
+        for i in range(n):
+            l, r = 0, n - 1
+            ok = True
+
+            while l <= r:
+                mid = (l + r) // 2
+
+                if mid == i:
+                    break
+
+                if mid < i:
+                    if arr[mid] >= arr[i]:
+                        ok = False
+                        break
+                    l = mid + 1
+                else:
+                    if arr[mid] <= arr[i]:
+                        ok = False
+                        break
+                    r = mid - 1
+
+            if ok:
+                ans += 1
+
+        return ans

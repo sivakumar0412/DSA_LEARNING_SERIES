f6e274013021f4ec3adf0e1630105f49ab60fa0c
sivakumar0412 <sivakumar041203@gmail.com>
2026-06-29 18:52:16 +0530

    S

--- pattern2.py
+++ pattern2.py
@@ -3187,3 +3187,20 @@ class Solution:
             dp[i] = (dp[i-1] + dp[i-m]) % MOD
 
         return dp[n]
+
+# Max Dot Product with 0 Insertions
+class Solution:
+    def maxDotProduct(self, a, b):
+        n = len(a)
+        m = len(b)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, min(i, m) + 1):
+                dp[i][j] = max(
+                    dp[i - 1][j],
+                    dp[i - 1][j - 1] + a[i - 1] * b[j - 1]
+                )
+
+        return dp[n][m]

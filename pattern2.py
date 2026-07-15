12cbbd381e643fa90eeac500ab443c4752edcd04
sivakumar0412 <sivakumar041203@gmail.com>
2026-07-15 21:29:34 +0530

    S

--- pattern2.py
+++ pattern2.py
@@ -3474,3 +3474,31 @@ class Solution:
             ans %= MOD   # keep result within MOD
 
         return ans
+# Longest Bitonic Subarray
+
+
+class Solution:
+    def bitonic(self, arr):
+        n = len(arr)
+
+        if n == 0:
+            return 0
+
+        inc = [1] * n
+        dec = [1] * n
+
+        # Increasing
+        for i in range(1, n):
+            if arr[i] >= arr[i - 1]:
+                inc[i] = inc[i - 1] + 1
+
+        # Decreasing
+        for i in range(n - 2, -1, -1):
+            if arr[i] >= arr[i + 1]:
+                dec[i] = dec[i + 1] + 1
+
+        ans = 1
+        for i in range(n):
+            ans = max(ans, inc[i] + dec[i] - 1)
+
+        return ans

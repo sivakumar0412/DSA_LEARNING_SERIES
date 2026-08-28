a7acba414f4855095bd27a3548ec22effae4128f
sivakumar0412 <sivakumar041203@gmail.com>
2026-08-28 13:03:27 +0530

    s

--- pattern2.py
+++ pattern2.py
@@ -3962,3 +3962,17 @@ class Solution:
                 ans = max(ans, sorted_height[j] * (j + 1))
 
         return ans
+
+# Minimum Cost Selection
+class Solution:
+
+    def minCost(self, mat):
+        dp = mat[0][:]
+        for i in range(1,len(mat)):
+            a,b,c = dp
+            dp =[
+                mat[i][0]+min(b,c),
+                mat[i][1]+min(a,c),
+                mat[i][2]+min(a,b)
+                ]
+        return min(dp)

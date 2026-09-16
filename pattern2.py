aefaa91254ffcf0cdd7b7705e6a99fb480b37f75
sivakumar0412 <sivakumar041203@gmail.com>
2026-09-16 11:44:35 +0530

    S

--- pattern2.py
+++ pattern2.py
@@ -4179,3 +4179,18 @@ class Solution:
                 break
 
         return ans
+
+# Dominant Pairs
+class Solution:
+    def dominantPairs(self, arr: list[int]) -> int:
+        # code here
+        n=len(arr)//2
+        first = sorted(arr[:n])
+        second = sorted(arr[n:])
+        j=0
+        ans=0
+        for x in first:
+            while j < n and x >=5*second[j]:
+                j+=1
+            ans+=j
+        return ans
